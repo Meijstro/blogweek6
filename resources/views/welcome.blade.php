5e9f8fda4b437c0e922e938f648aa418935dea9a
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-02-20 15:23:18 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome Thijs
                 </div>
 
                 <div class="links">

619b54156d6af93e653ef39c849b3b970e35b2dc
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-02-21 15:57:32 +0100

    extra's

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,9 @@
 @extends('master')
 
 @section('content')
+<label for="name">Filter on category:</label> <br><br>
 <select class="dropdown" name="category_selection" onchange="">
+  <option value="">All</option>
   <option value="fcgroningen">FC Groningen</option>
   <option value="cryptovaluta">Cryptovaluta</option>
   <option value="trump">Trump</option>

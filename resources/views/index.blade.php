66ac40a4008b82caa764e1a55e47272126f6affd
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-02-21 16:55:49 +0100

    niks

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,11 @@
 
 @section('content')
 <label for="name">Filter on category:</label> <br><br>
-<select class="dropdown" name="category_selection" onchange="">
-  <option value="">All</option>
-  <option value="fcgroningen">FC Groningen</option>
-  <option value="cryptovaluta">Cryptovaluta</option>
-  <option value="trump">Trump</option>
-</select>
+
+  <a href="fcgroningen">FC Groningen</a>
+  <a href="cryptovaluta">Cryptovaluta</a>
+  <a href="trump">Trump</a>
+
 <hr>
 
 

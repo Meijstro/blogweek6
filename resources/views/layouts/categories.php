45347d9d8316445aa5fca8d8d50df464745b1f04
Thijs Vunderink <thijsvunderink@gmail.com>
2018-02-22 15:24:10 +0100

    fixed routing

--- resources/views/layouts/categories.php
+++ resources/views/layouts/categories.php
@@ -2,8 +2,8 @@
   <h4> <label for="name" class="font-italic">Categories</label></h4>
   <ol class="list-unstyled mb-0">
     <li><a href="/">All</a></li>
-    <li><a href="fcgroningen">FC Groningen</a></li>
-    <li><a href="cryptovaluta">Cryptovaluta</a></li>
-    <li><a href="trump">Trump</a></li>
+    <li><a href="/fcgroningen">FC Groningen</a></li>
+    <li><a href="/cryptovaluta">Cryptovaluta</a></li>
+    <li><a href="/trump">Trump</a></li>
   </ol>
 </div>
